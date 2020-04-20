286a8ab5de0a9a0a8fa70d172bb7d0cd75df6652
Elina <49618499+ElinaIV@users.noreply.github.com>
2020-04-20 21:41:48 +0300

    Update HW10_sender.cpp

--- HW10/HW10_sender.cpp
+++ HW10/HW10_sender.cpp
@@ -1,4 +1,4 @@
-﻿#define BOOST_DATE_TIME_NO_LIB
+#define BOOST_DATE_TIME_NO_LIB
 
 #include <cstdlib>
 #include <cstring>
@@ -8,13 +8,9 @@
 #include <conio.h>
 #include <sys/types.h>
 #include <string>
-#include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/ipc/message_queue.hpp>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/containers/map.hpp>
-#include <boost/interprocess/containers/deque.hpp>
-#include <boost/container/detail/pair.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
@@ -23,23 +19,22 @@
 using segment_manager_t = boost::interprocess::managed_shared_memory::segment_manager;
 using char_allocator_t = boost::interprocess::allocator < char, segment_manager_t >;
 using string_t = boost::interprocess::basic_string < char, std::char_traits < char >, char_allocator_t>;
-using pid_t = int;
-using pair_t = std::pair <pid_t, string_t>;
-using record_t = std::pair<int, pair_t>;
-using pair_allocator_t = boost::interprocess::allocator <pair_t, segment_manager_t>;
+using id_t = std::size_t;
+using record_t = std::pair<const id_t, string_t>;
 using record_allocator_t = boost::interprocess::allocator <record_t, segment_manager_t>;
-using map_t = boost::interprocess::map <int, record_t, std::less <int>, record_allocator_t>;
+using map_t = boost::interprocess::map <id_t, string_t, std::less <id_t>, record_allocator_t>;
 
 using mx = boost::interprocess::interprocess_mutex;
 using cn = boost::interprocess::interprocess_condition;
 
-void waiter(map_t* map, size_t& keptSize, mx* mutex, cn* condition) {
+/* myMap <id, msg> */
+void waiter(map_t* map, id_t id, size_t& keptSize, mx* mutex, cn* condition) {
 	while (true) {
-		if (_getpid() != map->rbegin()->second.first) {
+		{
 			std::lock_guard lock(*mutex);
 
-			if (map->size() > keptSize) {
-				std::cout << map->rbegin()->second.second << std::endl;
+			if (map->rbegin()->first != keptSize) {
+				std::cout << map->at(id) << std::endl;
 				++keptSize;
 			}
 		}
@@ -56,13 +51,13 @@ int main() {
 	record_allocator_t alloc{ shared_memory.get_segment_manager() };
 	auto mutex = shared_memory.find_or_construct<mx>(mutex_name.c_str())();
 	auto condition = shared_memory.find_or_construct<cn>(condition_name.c_str())();
-	auto map = shared_memory.find_or_construct <map_t>("map")(std::less<int>(), alloc);
+	auto map = shared_memory.find_or_construct <map_t>("map")(std::less<id_t>(), alloc);
 
 	/* working */
 	std::string msg;
-	size_t keptSize = map->size();
-	int id = 0;
-	std::thread waiter(waiter, map, std::ref(keptSize), mutex, condition);
+	size_t keptSize = -1;
+	int keptId = 0;
+	std::thread waiter(waiter, map, keptId, std::ref(keptSize), mutex, condition);
 	while (std::getline(std::cin, msg)) {
 		if (msg == "/exit/") {
 			condition->notify_all();
@@ -70,9 +65,12 @@ int main() {
 			break;
 		}
 		else {
+			if (!map->empty()) {
+				keptSize = map->size;
+			}
 			std::unique_lock lock(*mutex);
 			string_t bMsg(msg.data(), shared_memory.get_segment_manager());
-			map->insert(std::pair(id++, std::pair(_getpid(), bMsg)));
+			map->insert(std::pair<id_t, string_t>(++keptSize, bMsg));
 			lock.unlock();
 			condition->notify_all();
 		}
