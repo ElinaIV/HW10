274e4908fd22af001337ddcbfde37b59673be48f
Elina <49618499+ElinaIV@users.noreply.github.com>
2020-04-26 00:22:09 +0300

    Update HW10_sender.cpp

--- HW10/HW10_sender.cpp
+++ HW10/HW10_sender.cpp
@@ -34,11 +34,11 @@ using mx = boost::interprocess::interprocess_mutex;
 using cn = boost::interprocess::interprocess_condition;
 
 /* myMap <id, msg> */
-void waiter(map_t* map, id_t id, int& keptId, mx* mutex, cn* condition, bool& exitCondition) {
+void waiter(map_t* map, id_t id, int& keptId, mx* mutex, cn* condition, std::atomic<bool>& exitCondition) {
 	while (true) {
 		{
 			std::unique_lock lock{ *mutex };
-			condition->wait(lock, [map, &keptId, &exitCondition]() { return !map->empty() || exitCondition && (std::prev(map->end()))->first != keptId ; });
+			condition->wait(lock, [map, &keptId, &exitCondition]() { return !map->empty() || exitCondition && (std::prev(map->end()))->first != keptId; });
 
 			if (exitCondition) {
 				break;
@@ -64,33 +64,35 @@ int main() {
 #endif // CLEAN_SHARED
 
 	/* creating */
-	boost::interprocess::managed_shared_memory shared_memory(boost::interprocess::open_or_create, shared_memory_name.c_str(), 8192);
+	boost::interprocess::managed_shared_memory shared_memory(boost::interprocess::open_or_create, shared_memory_name.c_str(), 8096);
 	std::string mutex_name = "mutex";
 	std::string condition_name = "condition";
 	record_allocator_t alloc{ shared_memory.get_segment_manager() };
 	auto mutex = shared_memory.find_or_construct<mx>(mutex_name.c_str())();
 	auto condition = shared_memory.find_or_construct<cn>(condition_name.c_str())();
 	auto map = shared_memory.find_or_construct <map_t>("map")(std::less<id_t>(), alloc);
-	auto exitCondition = shared_memory.find_or_construct < bool >("exitCondition")(false);
+	auto clientsOnline = shared_memory.find_or_construct < std::atomic<int> >("clientsOnline")(0);
 	std::ofstream logFile("messageLog.txt", std::ios::app);
 
 	/* working */
 	string_t msg{ shared_memory.get_segment_manager() };
 	int keptId = -1;
-
+	std::atomic<bool> exitCondition = false;
 	{
 		std::lock_guard lock(*mutex);
 		if (!map->empty()) {
 			keptId = map->size();
 		}
 	}
-
-	std::thread waiter(waiter, map, keptId, std::ref(keptId), mutex, condition, std::ref(*exitCondition));
+	(*clientsOnline)++;
+	std::cout << "Clients: " << *clientsOnline << std::endl;
+	std::thread waiter(waiter, map, keptId, std::ref(keptId), mutex, condition, std::ref(exitCondition));
 	while (boost::container::getline(std::cin, msg)) {
 		if (msg == "/exit/") {
-			*exitCondition = true;
+			exitCondition = true;
 			condition->notify_all();
 			waiter.join();
+			(*clientsOnline)--;
 			break;
 		}
 		else {
@@ -100,7 +102,12 @@ int main() {
 			condition->notify_all();
 		}
 	}
-	boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());
+	if (*clientsOnline == 0) {
+		std::cout << "Removed shared memory" << std::endl;
+		boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());
+	}
 	return EXIT_SUCCESS;
 }
+
+
 
